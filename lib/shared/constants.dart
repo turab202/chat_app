3ddd75420db01c4f8dc47104d9cb5760fc4afccc
turab202 <turabturqb@gmail.com>
2025-12-29 20:54:05 +0300

    Update constants.dart

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
 class Constants {
+  // Make these static
   static String appId = "1:150456166386:web:dbb56830b4098ab058ad6b";
   static String apiKey = "AIzaSyC6NNXL4aVMCJRp337UgyXZdWze2AvMBTI";
   static String messagingSenderId = "150456166386";
   static String projectId = "chatapp-157e6";
-  final primaryColor = const Color(0xFFee7b64);
+  
+  // Make this static too
+  static Color primaryColor = const Color(0xFFee7b64);
 }
